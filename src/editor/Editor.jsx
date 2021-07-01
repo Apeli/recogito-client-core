f26809cc0b3b23cfffd0fa397c6cbe4779bcb8fc
Aapo Virta <aapo@mediapool.fi>
2021-07-01 12:24:27 +0300

    Fix

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { Component } from 'react';
 import Draggable from 'react-draggable';
 import { getWidget, DEFAULT_WIDGETS } from './widgets';
 import { TrashIcon } from '../Icons';
 import setPosition from './setPosition';
 import i18n from '../i18n';
 
-/** We need to compare bounds by value, not by object ref **/
-const bounds = elem => {
-    const { top, left, width, height } = elem.getBoundingClientRect();
-    return `${top}, ${left}, ${width}, ${height}`;
-}
+/** Shorthand **/
+const toArray = body =>
+  Array.isArray(body) ? body : [ body ];
+
 
 /**
  * The popup editor component.
@@ -18,248 +17,300 @@ const bounds = elem => {
  * we could create a stack of revisions, and allow going back
  * with CTRL+Z.
  */
-const Editor = props => {
+export default class Editor extends Component {
 
-    // The current state of the edited annotation vs. original
-    const [currentAnnotation, setCurrentAnnotation] = useState();
-
-    const [dragged, setDragged] = useState(false);
+  constructor(props) {
+    super(props);
 
     // Reference to the DOM element, so we can set position
-    const element = useRef();
+    this.element = React.createRef();
 
-    // Set derived annotation state
-    useEffect(() => {
-        setCurrentAnnotation(props.annotation);
-    }, [props.annotation]);
+    this.state = {
+      currentAnnotation: props.annotation,
+      dragged: false
+    }
+  }
 
-    // Change editor position if element has moved
-    useEffect(() => {
-        if (element.current) {
-            // Note that ResizeObserver fires once when observation starts
-            return initResizeObserver();
-        }
-    }, [bounds(props.selectedElement)]);
+  componentWillReceiveProps(next) {
+    this.setState({ currentAnnotation: next.annotation });
 
-    useEffect(() => {
-        if (currentAnnotation)
-            setCurrentAnnotation(currentAnnotation.clone({ target: props.modifiedTarget }));
-    }, [props.modifiedTarget])
+    if (this.props.modifiedTarget != next.modifiedTarget) {
+      // Update in case target was changed (move, resize)
+      if (this.state.currentAnnotation) {
+        this.updateCurrentAnnotation({ target: this.props.modifiedTarget });
 
-    const initResizeObserver = () => {
-        if (window?.ResizeObserver) {
-            const resizeObserver = new ResizeObserver(() => {
-                if (!dragged)
-                    setPosition(props.wrapperEl, element.current, props.selectedElement);
-            });
+        // Change editor position if element has moved
+        this.removeObserver && this.removeObserver();
+        this.removeObserver = this.initResizeObserver();
+      }
+    }
+  }
 
-            resizeObserver.observe(props.wrapperEl);
-            return () => resizeObserver.disconnect();
-        } else {
-            // Fire setPosition *only* for devices that don't support ResizeObserver
-            if (!dragged)
-                setPosition(props.wrapperEl, element.current, props.selectedElement);
-        }
+  componentDidMount() {
+    // Init observer (triggers setPosition once)
+    this.removeObserver = this.initResizeObserver();
+  }
+
+  componentWillUnmount() {
+    // Remove the observer
+    this.removeObserver && this.removeObserver();
+  }
+
+  initResizeObserver = () => {
+    if (window?.ResizeObserver) {
+      const resizeObserver = new ResizeObserver(() => {
+        if (!this.state.dragged)
+          setPosition(this.props.wrapperEl, this.element.current, this.props.selectedElement);
+      });
+
+      resizeObserver.observe(this.props.wrapperEl);
+      return () => resizeObserver.disconnect();
+    } else {
+      // Fire setPosition manually *only* for devices that don't support ResizeObserver
+      if (!this.state.dragged)
+        setPosition(this.props.wrapperEl, this.element.current, this.props.selectedElement);
+    }  
+  }
+
+  /** Creator and created/modified timestamp metadata **/
+  creationMeta = body => {
+    const meta = {};
+
+    const { user } = this.props.env;
+
+    // Metadata is only added when a user is set, otherwise
+    // the Editor operates in 'anonymous mode'.
+    if (user) {
+      meta.creator = {};
+      if (user.id) meta.creator.id = user.id;
+      if (user.displayName) meta.creator.name = user.displayName;
+
+      meta[body.created ? 'modified' : 'created'] = this.props.env.getCurrentTimeAdjusted();
     }
 
-    // Creator and created/modified timestamp metadata
-    const creationMeta = body => {
-        const meta = {};
+    return meta;
+  }
 
-        const { user } = props.env;
+  /** Shorthand **/
+  updateCurrentAnnotation = (diff, saveImmediately) => {
+    this.setState({
+      currentAnnotation: this.state.currentAnnotation.clone(diff)
+    }, () => {
+      if (saveImmediately)
+        this.onOk();
+    })
+  }
 
-        // Metadata is only added when a user is set, otherwise
-        // the Editor operates in 'anonymous mode'.
-        if (user) {
-            meta.creator = {};
-            if (user.id) meta.creator.id = user.id;
-            if (user.displayName) meta.creator.name = user.displayName;
+  /**
+   * Appends a body to the current annotation. For convenience: also 
+   * takes an array of bodies as argument.
+   * @param bodyOrBodies the body or list of bodies to append
+   * @param saveImmediately set to true to save & close immediately
+   */
+  onAppendBody = (bodyOrBodies, saveImmediately) => {
+    const toAppend = toArray(bodyOrBodies).map(b => 
+      ({ ...b, ...this.creationMeta(b) }));
 
-            meta[body.created ? 'modified' : 'created'] = props.env.getCurrentTimeAdjusted();
-        }
+    this.updateCurrentAnnotation({ 
+      body: [ ...this.state.currentAnnotation.bodies, ...toAppend ] 
+    }, saveImmediately);
+  }
 
-        return meta;
+  /**
+   * Updates a previous body with the given body.
+   * @param previous the body to replace
+   * @param updated the body to replace with
+   * @param saveImmediately set to true to save & close immediately
+   */
+  onUpdateBody = (previous, updated, saveImmediately) => { 
+    this.updateCurrentAnnotation({
+      body: this.state.currentAnnotation.bodies.map(body => 
+        body === previous ? { ...updated, ...this.creationMeta(updated) } : body)
+    }, saveImmediately);
+  }
+
+  /** 
+   * For convenience: an 'append or update' shorthand.
+   */
+   onUpsertBody = (arg1, arg2, saveImmediately) => {
+    if (arg1 == null && arg2 != null) {
+      // Append arg 2 as a new body
+      this.onAppendBody(arg2, saveImmediately);
+    } else if (arg1 != null && arg2 != null) {
+      // Replace body arg1 with body arg2
+      this.onUpdateBody(arg1, arg2, saveImmediately);
+    } else if (arg1 != null && arg2 == null) {
+      // Find the first body with the same purpose as arg1,
+      // and upsert
+      const existing = this.state.currentAnnotation.bodies.find(b => b.purpose === arg1.purpose);
+      if (existing)
+        this.onUpdateBody(existing, arg1, saveImmediately);
+      else
+        this.onAppendBody(arg1, saveImmediately);
     }
+  }
 
-    const onAppendBody = body => {
+  /**
+   * Removes the given body from the current annotation. For 
+   * convenience: also takes an array of bodies as argument.
+   * @param bodyOrBodies the body or list of bodies to remove
+   * @param saveImmediately set to true to save & close immediately
+   */
+  onRemoveBody = (bodyOrBodies, saveImmediately) => {
+    const toRemove = toArray(bodyOrBodies);
 
-        // console.log("onAppendBody");
-        // console.log(body, Array.isArray(body));
-        // console.log(currentAnnotation);
-        // console.log(creationMeta(body));
-        // console.log("....");
-        // console.log([...currentAnnotation.bodies, { ...body, ...creationMeta(body) }]);
+    this.updateCurrentAnnotation({
+      body: this.state.currentAnnotation.bodies.filter(b => !toRemove.includes(b))
+    }, saveImmediately);
+  }
 
-        let bodyObj = [...currentAnnotation.bodies, { ...body, ...creationMeta(body) }];
-        if (Array.isArray(body)) {
-          bodyObj = body.map(body => {
-            return { ...body, ...creationMeta(body) }
-          });
-        }
+  /**
+   * For convenience: removes and appends one or more bodies
+   * in one go, optionally saving immediately.
+   */
+  onRemoveAndAppend = (bodyOrBodiesToRemove, bodyOrBodiesToAppend, saveImmediately) => {
+    const toRemove = toArray(bodyOrBodiesToRemove);
+    const toAppend = toArray(bodyOrBodiesToAppend).map(b => 
+      ({ ...b, ...this.creationMeta(b) }));
 
-        return setCurrentAnnotation(
-            currentAnnotation.clone({
-                body: bodyObj
-            })
-        );
+    this.updateCurrentAnnotation({
+      body: [
+        ...this.state.currentAnnotation.bodies.filter(b => !toRemove.includes(b)),
+        ...toAppend 
+      ]
+    }, saveImmediately);
+  }
+
+  /**
+   * Sets the given property value at the top level of the annotation.
+   * @param property property key
+   * @param value property value - set to null to delete the property
+   */
+  onSetProperty = (property, value) => {
+    // A list of properties the user is NOT allowed to set
+    const isForbidden = [ '@context', 'id', 'type', 'body', 'target' ].includes(property); 
+
+    if (isForbidden)
+      throw new Exception(`Cannot set ${property} - not allowed`);
+
+    if (value) {
+      this.updateCurrentAnnotation({ [ property ]: value });
+    } else {
+      const updated = this.currentAnnotation.clone();
+      delete updated[ property ];
+      this.setState({ currentAnnotation: updated });
     }
+  }
 
-    const onUpdateBody = (previous, updated) => {
-        // console.log("onUpdateBody", currentAnnotation, updated, creationMeta(updated));
-        return setCurrentAnnotation(
-            currentAnnotation.clone({
-                body: currentAnnotation.bodies.map(body =>
-                    body === previous ? { ...updated, ...creationMeta(updated) } : body)
-            })
-        )
+  onCancel = () => 
+    this.props.onCancel(this.props.annotation);
+
+  onOk = () => {
+    // Removes the state payload from all bodies
+    const undraft = annotation => 
+      annotation.clone({
+        body : annotation.bodies.map(({ draft, ...rest }) => rest)
+      });
+
+    const { currentAnnotation } = this.state;
+
+    // Current annotation is either a selection (if it was created from 
+    // scratch just now) or an annotation (if it existed already and was
+    // selected for editing)
+    if (currentAnnotation.bodies.length === 0 && !this.props.allowEmpty) {
+      if (currentAnnotation.isSelection)
+        onCancel();
+      else 
+        this.props.onAnnotationDeleted(this.props.annotation);
+    } else {
+      if (currentAnnotation.isSelection)
+        this.props.onAnnotationCreated(undraft(currentAnnotation).toAnnotation());
+      else
+        this.props.onAnnotationUpdated(undraft(currentAnnotation), this.props.annotation);
     }
+  }
 
-    const onRemoveBody = body => setCurrentAnnotation(
-        currentAnnotation.clone({
-            body: currentAnnotation.bodies.filter(b => b !== body)
-        })
-    );
+  onDelete = () => 
+    this.props.onAnnotationDeleted(this.props.annotation);
 
-    /** A convenience shorthand **/
-    const onUpsertBody = (arg1, arg2) => {
-        if (arg1 == null && arg2 != null) {
-            // Append arg 2 as a new body
-            onAppendBody(arg2);
-        } else if (arg1 != null && arg2 != null) {
-            // Replace body arg1 with body arg2
-            onUpdateBody(arg1, arg2);
-        } else if (arg1 != null && arg2 == null) {
-            // Find the first body with the same purpose as arg1,
-            // and upsert
-            const existing = currentAnnotation.bodies.find(b => b.purpose === arg1.purpose);
-            if (existing)
-                onUpdateBody(existing, arg1);
-            else
-                onAppendBody(arg1);
-        }
-    }
-
-    const onSetProperty = (property, value) => {
-        // A list of properties the user is NOT allowed to set
-        const isForbidden = ['@context', 'id', 'type', 'body', 'target'].includes(property);
-
-        if (isForbidden)
-            throw new Exception(`Cannot set ${property} - not allowed`);
-
-        if (value) {
-            setCurrentAnnotation(currentAnnotation.clone({
-                [property]: value }));
-        } else {
-            const updated = currentAnnotation.clone();
-            delete updated[property];
-            setCurrentAnnotation(updated);
-        }
-    };
-
-    const onCancel = () =>
-        props.onCancel(props.annotation);
-
-    const onOk = _ => {
-        // Removes the state payload from all bodies
-        const undraft = annotation =>
-            annotation.clone({
-                body: annotation.bodies.map(({ draft, ...rest }) => rest)
-            });
-
-        // Current annotation is either a selection (if it was created from 
-        // scratch just now) or an annotation (if it existed already and was
-        // opened for editing)
-        if (currentAnnotation.bodies.length === 0 && !props.allowEmpty) {
-            if (currentAnnotation.isSelection)
-                onCancel();
-            else
-                props.onAnnotationDeleted(props.annotation);
-        } else {
-            if (currentAnnotation.isSelection)
-                props.onAnnotationCreated(undraft(currentAnnotation).toAnnotation());
-            else
-                props.onAnnotationUpdated(undraft(currentAnnotation), props.annotation);
-        }
-    };
-
-    const onDelete = () =>
-        props.onAnnotationDeleted(props.annotation);
+  render() {
+    const { currentAnnotation } = this.state;
 
     // Use default comment + tag widget unless host app overrides
-    const widgets = props.widgets ?
-        props.widgets.map(getWidget) : DEFAULT_WIDGETS;
+    const widgets = this.props.widgets ? 
+      this.props.widgets.map(getWidget) : DEFAULT_WIDGETS;
 
     const isReadOnlyWidget = w => w.type.disableDelete ?
-        w.type.disableDelete(currentAnnotation, {
-            ...w.props,
-            readOnly: props.readOnly,
-            env: props.env
-        }) : false;
+      w.type.disableDelete(currentAnnotation, {
+        ...w.props,
+        readOnly:this.props.readOnly,
+        env: this.props.env
+      }) : false;
 
-    const hasDelete = currentAnnotation &&
-        // annotation has bodies or allowEmpty,
-        (currentAnnotation.bodies.length > 0 || props.allowEmpty) && // AND
-        !props.readOnly && // we are not in read-only mode AND
-        !currentAnnotation.isSelection && // this is not a selection AND
-        !widgets.some(isReadOnlyWidget); // every widget is deletable
+    const hasDelete = currentAnnotation && 
+      // annotation has bodies or allowEmpty,
+      (currentAnnotation.bodies.length > 0 || this.props.allowEmpty) && // AND
+      !this.props.readOnly && // we are not in read-only mode AND
+      !currentAnnotation.isSelection && // this is not a selection AND
+      !widgets.some(isReadOnlyWidget);  // every widget is deletable
 
     return (
-        <Draggable 
-      disabled={!props.detachable}
-      cancel=".r6o-btn, .r6o-nodrag" 
-      onDrag={() => setDragged(true)}>
+      <Draggable 
+        disabled={!this.props.detachable}
+        cancel=".r6o-btn, .r6o-nodrag" 
+        onDrag={() => this.setState({ dragged: true })}>
 
-      <div ref={element} className={dragged ? 'r6o-editor dragged' : 'r6o-editor'}>
-        <div className="r6o-arrow" />
-        <div className="r6o-editor-inner">
-          {widgets.map((widget, idx) => 
-            React.cloneElement(widget, { 
-              focus: idx === 0,
-              annotation : currentAnnotation,
-              readOnly : props.readOnly,
-              env: props.env,
-              onAppendBody,
-              onUpdateBody,
-              onRemoveBody,
-              onUpsertBody,
-              onSetProperty,
-              onSaveAndClose: onOk,
-              onClose: onOk,              
-            })
-          )}
-          
-          { props.readOnly ? (
-            <div className="r6o-footer">
-              <button
-                className="r6o-btn" 
-                onClick={onCancel}>{i18n.t('Close')}</button>
-            </div>
-          ) : (
-            <div className="r6o-footer">
-              { hasDelete && (
+        <div ref={this.element} className={this.state.dragged ? 'r6o-editor dragged' : 'r6o-editor'}>
+          <div className="r6o-arrow" />
+          <div className="r6o-editor-inner">
+            {widgets.map((widget, idx) => 
+              React.cloneElement(widget, { 
+                focus: idx === 0,
+                annotation : currentAnnotation,
+                readOnly : this.props.readOnly,
+                env: this.props.env,
+                onAppendBody: this.onAppendBody,
+                onUpdateBody: this.onUpdateBody,
+                onRemoveBody: this.onRemoveBody,
+                onUpsertBody: this.onUpsertBody,
+                onRemoveAndAppend: this.onRemoveAndAppend,
+                onSetProperty: this.onSetProperty,
+                onSaveAndClose: this.onOk              
+              })
+            )}
+            
+            { this.props.readOnly ? (
+              <div className="r6o-footer">
+                <button
+                  className="r6o-btn" 
+                  onClick={this.onCancel}>{i18n.t('Close')}</button>
+              </div>
+            ) : (
+              <div className="r6o-footer">
+                { hasDelete && (
+                  <button 
+                    className="r6o-btn left delete-annotation" 
+                    title={i18n.t('Delete')}
+                    onClick={this.onDelete}>
+                    <TrashIcon width={12} />
+                  </button>
+                )}
+
                 <button 
-                  className="r6o-btn left delete-annotation" 
-                  title={i18n.t('Delete')}
-                  onClick={onDelete}>
-                  <TrashIcon width={12} />
-                </button>
-              )}
+                  className="r6o-btn outline"
+                  onClick={this.onCancel}>{i18n.t('Cancel')}</button>
 
-              <button 
-                className="r6o-btn outline"
-                onClick={onCancel}>{i18n.t('Cancel')}</button>
-
-              <button 
-                className="r6o-btn "
-                onClick={onOk}>{i18n.t('Ok')}...</button>
-            </div>
-          )}
+                <button 
+                  className="r6o-btn "
+                  onClick={this.onOk}>{i18n.t('Ok')}</button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
 
-    </Draggable>
+      </Draggable>
     )
 
-}
+  }
 
-export default Editor;
+}
