e15cfa96054a6f71307791feb4196e92983974bd
Aapo Virta <aapo@mediapool.fi>
2021-07-13 17:07:13 +0300

    Bugfix from repo

--- src/editor/setPosition.js
+++ src/editor/setPosition.js
@@ -1,42 +1,24 @@
 /** Sets the editor position and determines a proper orientation **/
 const setPosition = (wrapperEl, editorEl, selectedEl) => {
-
-
-
-  console.log("setPosition START");
-  console.log("wrapperEl", wrapperEl, editorEl, selectedEl);
-
-
-  // Container element offset
+  // Container element bounds
   const containerBounds = wrapperEl.getBoundingClientRect();
-  const { pageYOffset } = window;
-
-  console.log(containerBounds, pageYOffset);
 
   // Re-set orientation class
   editorEl.className = 'r6o-editor';
 
-  // Set visible
+  // Make visible
   editorEl.style.opacity = 1;
 
   // Default orientation (upwards arrow, at bottom-left of shape)
   const { left, top, right, height, bottom } = selectedEl.getBoundingClientRect();
-
-  return;
-
   editorEl.style.top = `${top + height - containerBounds.top}px`;
   editorEl.style.left = `${left - containerBounds.left}px`;
 
-  console.log("ltrhb", left, top, right, height, bottom);
-
   const defaultOrientation = editorEl.children[1].getBoundingClientRect();
-
-  console.log("defO", defaultOrientation);
 
   // Test 1: does right edge extend beyond the width of the page?
   // If so, flip horizontally
   if (defaultOrientation.right > window.innerWidth) {
-    console.log("test 1")
     editorEl.classList.add('align-right');
     editorEl.style.left = `${right - defaultOrientation.width - containerBounds.left}px`;
   }
@@ -44,25 +26,21 @@ const setPosition = (wrapperEl, editorEl, selectedEl) => {
   // Test 2: does the bottom edge extend beyond the height of the page?
   // If so, flip vertically
   if (defaultOrientation.bottom > window.innerHeight) {
-    console.log("test 2")
-    // Flip vertically
-    const annotationTop = top + pageYOffset; // Annotation bottom relative to parents
-    const containerHeight = containerBounds.bottom + pageYOffset;
-
     editorEl.classList.add('align-bottom');
-    editorEl.style.top = 'auto';
-    editorEl.style.bottom = `${containerHeight - annotationTop}px`;
+    
+    const editorHeight = editorEl.children[1].getBoundingClientRect().height;
+    editorEl.style.top = `${top - containerBounds.top - editorHeight}px`;
   }
 
-  // Get bounding box in current orientation 
+  // Get bounding box in current orientation for next tests
   const currentOrientation = editorEl.children[1].getBoundingClientRect();
 
   // Test 3: does the top (still?) extend beyond top of the page?
-  // If so, push down
+  // If so, push it down
   if (currentOrientation.top < 0) {
-    console.log("test 3")
+    editorEl.classList.add('pushed-down');
+
     editorEl.style.top = `${-containerBounds.top}px`;
-    editorEl.style.bottom = 'auto';
 
     const shapeBottom = bottom - containerBounds.top;
     const editorBottom = currentOrientation.height - containerBounds.top;
@@ -73,13 +51,8 @@ const setPosition = (wrapperEl, editorEl, selectedEl) => {
 
   // Test 4: does the left edge extend beyond the start of the page?
   // If so, push inward
-  if (currentOrientation.left < 0) {
-    console.log("test 4")
+  if (currentOrientation.left < 0)
     editorEl.style.left = `${-containerBounds.left}px`;
-  }
-
-  alert(1);
-
 }
 
 export default setPosition;
